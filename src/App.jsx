48a0f78e80eb3fe1d1b355775d9b08d6ce04a4d9
Gajitmaglan <91707856+Gajitmaglan@users.noreply.github.com>
2023-07-22 19:00:16 +0300

    Update App.jsx back to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Products from './components/Products';
 import Header from './components/Header';
