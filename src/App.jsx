056c48c2d1ed0bfc0a2b09a176cd7ad3b8b6b17f
Gajitmaglan <91707856+Gajitmaglan@users.noreply.github.com>
2023-07-22 18:57:38 +0300

    Update App.jsx: change BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Products from './components/Products';
 import Header from './components/Header';
